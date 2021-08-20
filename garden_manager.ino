3565616ccabfd0fa8089d37ac18a345fa7c75e09
root <valiamin21@gmail.com>
2021-08-20 19:06:21 +0430

    watering by humidity

--- garden_manager.ino
+++ garden_manager.ino
@@ -11,12 +11,12 @@ const int indicatorSize = 7;
 const int indicatorDigits[indicatorSize] = {2, 3, 4, 5, 6, 7, 8};
 
 const unsigned long wateringTime = HOUR; // 1 hour by milliseconds
-const unsigned long wateringGapTime = 24UL * HOUR - wateringTime;
 
-
-bool watering;
+bool watering = false;
 unsigned long t0;
-
+const int HUMIDITY_TO_WATERING = 952;
+int dryCounter = 0;
+const int DRYCOUNTERLIMIT = 7;
 
 void openValve1();
 void closeValve1();
@@ -34,58 +34,69 @@ void setup() {
     pinMode(indicatorDigits[i], OUTPUT);
   }
 
-  openValve1();
-  openValve2();
-  t0 = millis();
+  closeValves();
 
   Serial.begin(9600);
 }
 
 void loop() {
+  int humidity = analogRead(dirtHumidityPin);
+  if(humidity >= HUMIDITY_TO_WATERING){
+    if(dryCounter < DRYCOUNTERLIMIT){
+      dryCounter++;
+    }
+  }else{
+    dryCounter = 0;
+  }
+
+  
 
   if(watering){
     if(millis() - t0 >= wateringTime){
-      closeValve1();
-      closeValve2();
-      t0 = millis();
+     closeValves();
     }
   }else{
-    if(millis() - t0 >= wateringGapTime){
-      openValve1();
-      openValve2();
+    if(dryCounter == DRYCOUNTERLIMIT){
+      openValves();
       t0 = millis();
     }
   }
 
-  int humidity = analogRead(dirtHumidityPin);
-  humidity /= 8;
   Serial.println(humidity);
+  humidity /= 8;
   showHumidityValue(humidity);
-  delay(30000);
+  delay(1000);
+}
+
+void openValves(){
+  openValve1();
+  openValve2();
+  watering = true;
+}
+
+void closeValves(){
+  closeValve1();
+  closeValve2();
+  watering = false;
 }
 
 void openValve1(){
-  watering = true;
   digitalWrite(valve1, LOW);
 }
 
 void closeValve1(){
-  watering = false;
   digitalWrite(valve1, HIGH);
 }
 
 void openValve2(){
-  watering = true;
   digitalWrite(valve2, LOW);
 }
 
 void closeValve2(){
-  watering = false;
   digitalWrite(valve2, HIGH);
 }
 
 void showHumidityValue(int x){
-  
   int p = 0;
   while(x != 0){
     int d = x % 2;
